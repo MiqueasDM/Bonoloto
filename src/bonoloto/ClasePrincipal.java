c214b19593174a24c04283193e37eace5ef07753
MiqueasDelgadoMorales <miqueasdm2001@gmail.com>
2020-05-05 23:11:54 +0200

    Cambio en la clase principal, un comentario nuevo.

--- src/bonoloto/ClasePrincipal.java
+++ src/bonoloto/ClasePrincipal.java
@@ -10,6 +10,7 @@ public class ClasePrincipal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Cambio en el codigo para crear rama
         System.out.println("Bonoloto:");
         Bonoloto b=new Bonoloto();
         b.crearBonoloto();
